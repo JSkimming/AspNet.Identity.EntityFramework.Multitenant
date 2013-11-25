e8730811deb23f545b65daa19856fd8df9923395
James Skimming <development@skimming.net>
2013-11-25 18:46:30 +0000

    Added integer primary key implementation

---
+++ src/Examples/IntegerPkImplementation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IntegerPkImplementation.Startup))]
+namespace IntegerPkImplementation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
