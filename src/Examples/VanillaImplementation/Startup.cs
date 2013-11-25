68ba33b72ddf2d066bbfa1edfa65fbdc8392c14d
James Skimming <development@skimming.net>
2013-11-25 18:32:15 +0000

    Added VanillaImplementation

---
+++ src/Examples/VanillaImplementation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VanillaImplementation.Startup))]
+namespace VanillaImplementation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
