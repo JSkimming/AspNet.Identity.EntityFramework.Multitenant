3d6be343eeb11807855027f83cf98ebfd524678e
James Skimming <development@skimming.net>
2014-01-30 18:46:07 +0000

    Updated StyleCop to 4.7.48.0

--- src/Settings.StyleCopEdit.cmd
+++ src/Settings.StyleCopEdit.cmd
@@ -1,1 +1,1 @@
-"%~dp0packages\StyleCop.MSBuild.4.7.47.0\tools\StyleCopSettingsEditor.exe" "%~dp0Settings.StyleCop"
+"%~dp0packages\StyleCop.MSBuild.4.7.48.0\tools\StyleCopSettingsEditor.exe" "%~dp0Settings.StyleCop"
